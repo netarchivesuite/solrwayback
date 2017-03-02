2f53ea65b9efaf67fc94b38263ad9532df65c038
nig <nig@statsbiblioteket.dk>
2017-03-02 09:48:10 +0100

    Slide in slide out details

--- src/main/webapp/js/calendar-widget/src/index.js
+++ src/main/webapp/js/calendar-widget/src/index.js
@@ -57,51 +57,57 @@ Vue.component('harvest-date', {
                 </p>
                 <p>Total harvests: <strong>{{ harvestData.numberOfHarvests | formatted-number }}</strong></p>
                 
-                <!--<transition-group name="slide">-->
-                <table class="monthLabels">
-                    <tr><td class="empty">&nbsp;</td></tr>
-                    <tr><td>January</td></tr>
-                    <tr><td>February</td></tr>
-                    <tr><td>March</td></tr>
-                    <tr><td>April</td></tr>
-                    <tr><td>May</td></tr>
-                    <tr><td>June</td></tr>
-                    <tr><td>July</td></tr>
-                    <tr><td>August</td></tr>
-                    <tr><td>September</td></tr>
-                    <tr><td>October</td></tr>
-                    <tr><td>November</td></tr>
-                    <tr><td>December</td></tr>
-                </table>
-                <table v-for="(months, year) in harvestData.dates"  v-bind:key="months">
-                    <thead>
-                        <tr>
-                            <th>{{ year }}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(data, month) in months" v-bind:key="data" >
-                            <td v-on:click="showDays(year,month)" v-tooltip.top-center="'Harvests: ' + data.numberOfHarvests.toLocaleString()" v-bind:class="{activityLevel4: data.activityLevel === 4, activityLevel3: data.activityLevel === 3, activityLevel2: data.activityLevel === 2, activityLevel1: data.activityLevel === 1}">&nbsp;</td>
-                        </tr>
-                    </tbody>
-                </table>              
-                <!--</transition-group>-->
-                <!--<transition-group name="slide">-->
-                    <div v-if="showDetails" id="details">
+                <transition name="slideLeft">
+                    <div v-if="!showDetails" class="yearTables">
+                        <table class="monthLabels">
+                            <tr><td class="empty">&nbsp;</td></tr>
+                            <tr><td>January</td></tr>
+                            <tr><td>February</td></tr>
+                            <tr><td>March</td></tr>
+                            <tr><td>April</td></tr>
+                            <tr><td>May</td></tr>
+                            <tr><td>June</td></tr>
+                            <tr><td>July</td></tr>
+                            <tr><td>August</td></tr>
+                            <tr><td>September</td></tr>
+                            <tr><td>October</td></tr>
+                            <tr><td>November</td></tr>
+                            <tr><td>December</td></tr>
+                        </table>
+                        <table v-for="(months, year) in harvestData.dates">
+                            <thead>
+                                <tr>
+                                    <th>{{ year }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="(data, month) in months">
+                                    <td v-on:click="showDays(year,month)" v-tooltip.top-center="'Harvests: ' + data.numberOfHarvests.toLocaleString()" v-bind:class="{activityLevel4: data.activityLevel === 4, activityLevel3: data.activityLevel === 3, activityLevel2: data.activityLevel === 2, activityLevel1: data.activityLevel === 1}">&nbsp;</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>                
+                </transition>        
+                <transition name="slideRight">
+                <div v-if="showDetails" class="detailsContainer">
+                    <div id="details">
                         <div v-on:click="showDetails = false" class="hideDetails">Hide details</div>
                         <h3>Details for {{ month | month-name }} - {{ year }}</h3>
-                        <ul v-for="day in harvestData['dates'][year][month]['days']" v-bind:key="day">
-                            <li v-for="harvest in day" v-bind:key="harvest">{{ harvest }}</li>
+                        <ul v-for="day in harvestData['dates'][year][month]['days']">
+                            <li v-for="harvest in day">{{ harvest }}</li>
                         </ul>
-                    </div>            
-                <!--</transition-group> -->  
-                <div id="legends">
-                    Less <div class="legend legend0"></div>
-                    <div class="legend legend1"></div>
-                    <div class="legend legend2"></div>
-                    <div class="legend legend3"></div>
-                    <div class="legend legend4"></div> More
-                </div>    
+                    </div>
+                 </div>               
+                </transition> 
+                <transition name="slideLeft">  
+                    <div v-if="!showDetails" id="legends">
+                        Less <div class="legend legend0"></div>
+                        <div class="legend legend1"></div>
+                        <div class="legend legend2"></div>
+                        <div class="legend legend3"></div>
+                        <div class="legend legend4"></div> More
+                    </div>
+                </transition>   
             </div>
             <template v-else>
                 <div id="spinner">
