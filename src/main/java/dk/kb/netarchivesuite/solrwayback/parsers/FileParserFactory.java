3e65d0d468368dc65f4d0081101a127ecf966d7a
Thomas Egense <thomas.egense@gmail.com>
2017-10-30 09:06:19 +0100

    better log

--- src/main/java/dk/kb/netarchivesuite/solrwayback/parsers/FileParserFactory.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/parsers/FileParserFactory.java
@@ -4,30 +4,30 @@ import dk.kb.netarchivesuite.solrwayback.service.dto.ArcEntry;
 
 public class FileParserFactory {
 
-    public static ArcEntry getArcEntry(String arcFilePath, long offset) throws Exception{
+    public static ArcEntry getArcEntry(String source_file_path, long offset) throws Exception{
 
-        if (arcFilePath == null ){           
-            throw new IllegalArgumentException("arcFilePath is null");        
+        if (source_file_path == null ){           
+            throw new IllegalArgumentException("source_file_path is null");        
         }
 
         ArcEntry arcEntry = null; 
 
-        if (arcFilePath.toLowerCase().endsWith(".warc")){
-            arcEntry = WarcParser.getWarcEntry(arcFilePath, offset);     
+        if (source_file_path.toLowerCase().endsWith(".warc")){
+            arcEntry = WarcParser.getWarcEntry(source_file_path, offset);     
         }
-        else if (arcFilePath.toLowerCase().endsWith(".warc.gz")){ //Same parser
-          arcEntry = WarcParser.getWarcEntry(arcFilePath, offset);     
+        else if (source_file_path.toLowerCase().endsWith(".warc.gz")){ //Same parser
+          arcEntry = WarcParser.getWarcEntry(source_file_path, offset);     
         }
                 
-        else if (arcFilePath.toLowerCase().endsWith(".arc")){
-            arcEntry = ArcParser.getArcEntry(arcFilePath, offset);
+        else if (source_file_path.toLowerCase().endsWith(".arc")){
+            arcEntry = ArcParser.getArcEntry(source_file_path, offset);
         }
-        else if (arcFilePath.toLowerCase().endsWith(".arc.gz")){ //Same parser
-          arcEntry = ArcParser.getArcEntry(arcFilePath, offset);
+        else if (source_file_path.toLowerCase().endsWith(".arc.gz")){ //Same parser
+          arcEntry = ArcParser.getArcEntry(source_file_path, offset);
         }       
         
         else{
-            throw new IllegalArgumentException("File not arc or warc:"+arcFilePath);
+            throw new IllegalArgumentException("File not arc or warc:"+source_file_path);
         }
 
         return arcEntry;
