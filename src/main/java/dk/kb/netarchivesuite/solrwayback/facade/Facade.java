4a3d4666ab932a39e0e281d6733a3157426971fc
Thomas Egense <thomas.egense@gmail.com>
2018-05-23 13:36:10 +0200

    stats for correct count on hits when grouping on url

--- src/main/java/dk/kb/netarchivesuite/solrwayback/facade/Facade.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/facade/Facade.java
@@ -677,6 +677,8 @@ public class Facade {
                                   //.queryParam( "fq","{!collapse%20field=url}")   //Only 1 hit from each URL, does not work in cloud                                    
                                   .queryParam( "group","true")
                                   .queryParam( "group.field","url")
+                                  .queryParam("stats",  "true")
+                                  .queryParam("stats.field",  "{!cardinality=true}url")
                                   .queryParam( "group.format","simple")
                                   .queryParam( "group.limit","1");
                                   
