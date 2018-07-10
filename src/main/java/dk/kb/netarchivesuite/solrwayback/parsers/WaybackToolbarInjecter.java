954f2f481e7305c09bc1fb92edf74ddf2c5e7c64
nig <nig@statsbiblioteket.dk>
2018-07-10 12:47:28 +0200

    Toolbox closed, adjustment.

--- src/main/java/dk/kb/netarchivesuite/solrwayback/parsers/WaybackToolbarInjecter.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/parsers/WaybackToolbarInjecter.java
@@ -173,10 +173,10 @@ public class WaybackToolbarInjecter {
     "       <style>" +
     "       #tegModal{z-index: 999999 !important; color: black; font-size: 14px;" + "font-family: arial, Helvetica,sans-serif;background: #ffffff; border: 1px solid black;border-radius: 4px; " +
     "       box-shadow: 0 0 5px 5px #ccc; display: block; left: calc(50% - 450px); opacity: 1; padding: 1.5em 1.5em .5em;" +
-            "position:fixed; text-align:left !important; top: 25%; width: 900px; z-index: 500;" +
+            "position:fixed; text-align:left !important; top: 25%; width: 900px; z-index: 500; box-sizing: content-box;" +
     "       transition: left 0.4s, opacity 0.3s, padding 0.3s, top 0.4s, width 0.3s;}" +
             "#tegModal p, #tegModal div{color: black !important; font-family: Arial, Helvetica, sans-serif; font-size: 12px !important}" +
-    "       #tegModal.closed {box-shadow: 0 0 0 0; left: 3px;opacity: 0.8; padding:0.7em; top: 3px; width: 15px; text-orientation: upright; writing-mode: vertical-rl;}" +
+    "       #tegModal.closed {box-shadow: 0 0 0 0; left: 3px;opacity: 0.8; padding:0.5em; top: 3px; width: auto; text-orientation: upright; writing-mode: vertical-rl;}" +
     "       #toggleToolbar, #closeToolbar{float: right; margin: -.8em -.5em 2em 2em;}" +
     "       #tegModal.closed #toggleToolbar{float: none; margin: 0;}" +
     "       #toggleToolbar{margin-left: 1em;}" +
