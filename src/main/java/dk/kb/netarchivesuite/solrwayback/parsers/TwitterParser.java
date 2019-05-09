0410eb71cbe1ee132b94963aae8921f9e311298c
Thomas Egense <thomas.egense@gmail.com>
2019-05-09 13:06:15 +0200

    Quick fix of missing images in playback for twitter JSON. Needs refactoring or rewrite. But is working now at least.

--- src/main/java/dk/kb/netarchivesuite/solrwayback/parsers/TwitterParser.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/parsers/TwitterParser.java
@@ -9,9 +9,13 @@ import java.util.Locale;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TwitterParser {
 
+  private static final Logger log = LoggerFactory.getLogger(TwitterParser.class);
+  
   private String author;
   private Date createDate;
   private ArrayList<String> imageUrlsList;
@@ -24,17 +28,30 @@ public class TwitterParser {
   private boolean retweet = false;
   
   public TwitterParser(String json) throws Exception{
+    log.info("parsing twitter json");
     
     imageUrlsList = new  ArrayList<String>();
     hashTagsList = new  ArrayList<String>();
 
     JSONObject full = new JSONObject(json);                
+    
+    
+    JSONObject extended =null;
+    
+    if (full.has("extended_entities")) {
+      extended = full.getJSONObject("extended_entities");
+      log.info("has extended entities");
+    }
+    
+    
     this.text="";
 
-    if (full.has("full_text")){
+    if (full.has("full_text") ){
       //System.out.println("fulltext case");
       this.text = full.getString("full_text");
-    }    
+    }else if(extended != null && extended.has("full_text")){
+      this.text=extended.getString("full_text");      
+    }        
     else{
       //System.out.println("text case");
       this.text =full.getString("text"); //legacy 
@@ -52,32 +69,45 @@ public class TwitterParser {
       numberOfRetweets = full.getInt("retweet_count");
     }
     
-    if (full.has("retweeted_status")){
-      this.retweet=true;
-    }    
-            
+   
+    
+    //TODO rewrite to JSONUtil
     JSONObject entities; // Getting the entities require many special cases. Sometimes they are double, need to read into specification
 
     if (full.has("retweeted_status")) {
      this.retweet=true;
-      
+      log.info("has retweeted_status");
       JSONObject retweet = full.getJSONObject("retweeted_status");
       this.text= retweet.getString("text");
-      if (retweet.has("extended_tweet")){
-        entities = retweet.getJSONObject("extended_tweet").getJSONObject("entities");
+      if (retweet.has("extended_entities")){
+        entities = retweet.getJSONObject("extended_entities");
+        log.info("retweet extended entities");
       }
       else{
+        log.info("retweet normal entities");
         entities = retweet.getJSONObject("entities");
       }           
+    }else if(extended != null && extended.has("entities")){
+      entities = extended.getJSONObject("entities");      
+      log.info("extended entities");
+    }
+    else if (full.has("extended_tweet")){     
+      entities = full.getJSONObject("extended_tweet").getJSONObject("entities");            
+      log.info("normal from extended tweet");
+    }    
+    else if (full.has("extended_entities")){     
+      entities = full.getJSONObject("extended_entities");            
+      log.info("extended entities (from full)");
     }
     else if (full.has("entities")){     
       entities = full.getJSONObject("entities");            
-      //System.out.println("entities case");
+      log.info("entities (from full)");
     }
     else{
       throw new Exception("could not find entities on twitter JSON");
     }
 
+    
     //media(images), not always there.
     if (entities.has("media")){
       JSONArray media = entities.getJSONArray("media");   
@@ -92,7 +122,8 @@ public class TwitterParser {
       }               
     }
 
-    JSONArray hashTags = entities.getJSONArray("hashtags");
+    //Not sure always to take hash tags here
+    JSONArray hashTags = full.getJSONObject("entities").getJSONArray("hashtags");
 
     for (int i = 0;i<hashTags.length();i++){ //keywords
       String tag =  ((JSONObject) hashTags.get(i)).getString("text");
