1aa389035e637c360fd1186640c4142c015e721b
Thomas Egense <thomas.egense@gmail.com>
2019-07-18 13:40:43 +0200

    indexdoc new field extracted:content_type_full Ignore revisits when resolving leaks

--- src/main/java/dk/kb/netarchivesuite/solrwayback/solr/NetarchiveSolrClient.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/solr/NetarchiveSolrClient.java
@@ -50,7 +50,7 @@ public class NetarchiveSolrClient {
   protected  static NetarchiveSolrClient instance = null;
   protected  static Pattern TAGS_VALID_PATTERN = Pattern.compile("[-_.a-zA-Z0-9Ã¦Ã¸Ã¥Ã†Ã˜Ã…]+"); 
 
-  protected static String indexDocFieldList = "id,score,title,url,url_norm,links_images,source_file_path,source_file,source_file_offset,domain,resourcename,content_type,content_type_norm,hash,type,crawl_date,content_encoding,exif_location,status_code,last_modified,redirect_to_norm";
+  protected static String indexDocFieldList = "id,score,title,url,url_norm,links_images,source_file_path,source_file,source_file_offset,domain,resourcename,content_type,content_type_full,content_type_norm,hash,type,crawl_date,content_encoding,exif_location,status_code,last_modified,redirect_to_norm";
 
   protected NetarchiveSolrClient() { // private. Singleton
   }
@@ -811,6 +811,7 @@ return docs;
     solrQuery.set("group.size", "10");
     solrQuery.set("group.sort","abs(sub(ms("+timeStamp+"), crawl_date)) asc");
     solrQuery.add("fl", indexDocFieldList);
+    solrQuery.setFilterQueries("record_type:response"); //No binary for revists.     
     QueryResponse rsp = solrServer.query(solrQuery, METHOD.POST);
     SolrDocumentList docs = groupsToDoc(rsp);
     return solrDocList2IndexDoc(docs);    
@@ -907,6 +908,7 @@ return docs;
     indexDoc.setType((String) doc.get("type"));
     indexDoc.setExifLocation((String) doc.get("exif_location"));        
     indexDoc.setRedirectToNorm((String) doc.get("redirect_to_norm"));
+    indexDoc.setContent_type_full((String) doc.get("content_type_full"));
     Date dateModified = (Date) doc.get("last_modified");
     if (dateModified != null){    
       indexDoc.setLastModifiedLong(dateModified.getTime());
