5bc717606386f9965aca4075e9284c635aa6a2f4
jorntx <jt@kb.dk>
2020-08-25 15:14:48 +0200

    fixed broken image paths, fixed randomly corrupted webpack build output

--- src/js/vue.config.js
+++ src/js/vue.config.js
@@ -2,12 +2,12 @@ process.env.VUE_APP_VERSION = process.env.NODE_ENV === 'production' ? require('.
 module.exports = {
   devServer: {
     proxy: {
-    '/services/frontend/solr/': {
+    '^/services/frontend/solr/': {
       target: 'http://localhost:8080/solrwayback/services/frontend/solr/',
       pathRewrite: { '^/services/frontend/solr/': '' },
       changeOrigin: true
       },
-    '/services/frontend/images/htmlpage/': {
+    '^/services/frontend/images/htmlpage/': {
       target: 'http://localhost:8080/solrwayback/services/frontend/images/htmlpage/',
       pathRewrite: { '^/services/frontend/images/htmlpage/': '' },
       changeOrigin: true
@@ -19,13 +19,6 @@ publicPath: process.env.NODE_ENV === 'production'
 : '/',
 assetsDir: 'static',
 
+indexPath: 'solrwayback_index_page.html' 
 
-chainWebpack: (config) => {
-  if (process.env.NODE_ENV === 'production') {
-      config.plugin('html').tap((opts) => {
-          opts[0].filename = './solrwayback_index_page.html'
-          return opts
-      })
-  }
-},
 }
