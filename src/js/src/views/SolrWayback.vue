78c89d61277521e0c68b0594eb168037fd3d6cce
Jesper Lauridsen <jla@kb.dk>
2020-09-04 16:14:50 +0200

    make sure we do our stuff when route change is done.

--- src/js/src/views/SolrWayback.vue
+++ src/js/src/views/SolrWayback.vue
@@ -22,7 +22,8 @@ export default {
   computed: {
     ...mapState({
       searchAppliedFacets: state => state.Search.searchAppliedFacets,
-      query: state => state.Search.query
+      query: state => state.Search.query,
+      solrSettings: state => state.Search.solrSettings
     }),
   },
   methods: {
@@ -30,15 +31,35 @@ export default {
       requestSearch: 'requestSearch',
       requestFacets: 'requestFacets',
       updateQuery: 'updateQuery',
+      requestImageSearch: 'requestImageSearch',
       updateSearchAppliedFacets:'updateSearchAppliedFacets',
+      updateSolrSettingGrouping:'updateSolrSettingGrouping',
+      updateSolrSettingImgSearch:'updateSolrSettingImgSearch',
+      updateSolrSettingUrlSearch:'updateSolrSettingUrlSearch'
     }),
   },
   beforeRouteUpdate (to, from, next) {
-    console.log('we updated route')
+    console.log('we updated route', to)
     this.updateQuery(to.query.q)
+    to.query.grouping === 'true' ? this.updateSolrSettingGrouping(true) : this.updateSolrSettingGrouping(false)
+    to.query.imgSearch === 'true' ? this.updateSolrSettingImgSearch(true) : this.updateSolrSettingImgSearch(false)
+    to.query.urlSearch === 'true' ? this.updateSolrSettingUrlSearch(true) :  this.updateSolrSettingUrlSearch(false)
     to.query.facets ? this.updateSearchAppliedFacets(to.query.facets) : this.updateSearchAppliedFacets('')
-    this.requestSearch({query:this.query, facets: this.searchAppliedFacets})
-    this.requestFacets({query:this.query, facets: this.searchAppliedFacets})
+    if(this.solrSettings.imgSearch === true) {
+          console.log('firing image search')
+           this.requestImageSearch({query:this.query})
+          return
+        }
+        if(this.solrSettings.urlSearch === true) {
+          console.log('fireing url search')
+          return
+        }
+        else {
+          console.log('firing normal search')
+          this.requestSearch({query:this.query, facets:this.searchAppliedFacets, options:this.solrSettings})
+          this.requestFacets({query:this.query, facets:this.searchAppliedFacets, options:this.solrSettings})
+          return
+        }
   } 
 }
 </script>
