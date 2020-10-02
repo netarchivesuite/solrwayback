b0b7093143f794923cda25b5359f1a1a826a67fd
Jesper Lauridsen <jla@kb.dk>
2020-10-02 13:05:46 +0200

    first stab

---
+++ src/js/src/components/AboutComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="aboutContainer">
+    test
+  </div>
+</template>
+
+<script>
+
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'AboutComponent',
+  
+  computed: {
+    ...mapState({
+    }),
+  },
+  mounted () {
+  },
+ 
+  methods: {
+    ...mapActions('Search', {
+    }),
+  }
+}
+
+</script>
+
+    
