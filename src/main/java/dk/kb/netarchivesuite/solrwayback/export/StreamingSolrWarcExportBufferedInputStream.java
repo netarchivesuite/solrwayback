c69c72c9e22eecf397f90829269b2c61c06e68ee
Toke Eskildsen <toes@kb.dk>
2020-10-28 13:13:02 +0100

    Switch to stream-based buffering for WARC export

--- src/main/java/dk/kb/netarchivesuite/solrwayback/export/StreamingSolrWarcExportBufferedInputStream.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/export/StreamingSolrWarcExportBufferedInputStream.java
@@ -1,9 +1,13 @@
 package dk.kb.netarchivesuite.solrwayback.export;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import dk.kb.netarchivesuite.solrwayback.solr.SolrGenericStreaming;
 import org.apache.solr.common.SolrDocument;
@@ -12,50 +16,62 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dk.kb.netarchivesuite.solrwayback.parsers.ArcHeader2WarcHeader;
-import dk.kb.netarchivesuite.solrwayback.parsers.ArcParser;
 import dk.kb.netarchivesuite.solrwayback.parsers.ArcParserFileResolver;
 import dk.kb.netarchivesuite.solrwayback.parsers.WarcParser;
 import dk.kb.netarchivesuite.solrwayback.service.dto.ArcEntry;
-import dk.kb.netarchivesuite.solrwayback.solr.SolrStreamingWarcExportClient;
 
 public class StreamingSolrWarcExportBufferedInputStream extends InputStream{
 
   private static final Logger log = LoggerFactory.getLogger(StreamingSolrWarcExportBufferedInputStream.class);
 
-  private int index;
-  private List<byte[]> inputBuffer = new ArrayList<>();
-  private int maxRecords;
+  private final SolrGenericStreaming solrClient;
+  private final int maxRecords;
+  private final List<InputStream> entryStreams = new ArrayList<>(); // Ideally a FIFO buffer, but not worth the hassle
   private int docsWarcRead;
   private int docsArcRead;
-  private SolrGenericStreaming solrClient;
 
 
   @Override
-  public int read(){
+  public int read() throws IOException {
+    // Single byte read is not optimized: Callers should use read(byte[] b, int off, int len) if speed is a factor
+    int read = read(SINGLE_BYTE, 0, 1);
+    return read == -1 ? -1 : 0xff & SINGLE_BYTE[0];
+  }
+  private final byte[] SINGLE_BYTE = new byte[1];
 
-    if (inputBuffer.size() == 0) {
-      loadMore();
+  @Override
+  public int read(byte[] b, int off, final int len) throws IOException {
+    int totalRead = 0;
+    while (totalRead < len) {
+      int toRead = len-totalRead;
+
+      // Do we have any content?
+      if (entryStreams.isEmpty()) {
+        loadMore();
+        if (entryStreams.isEmpty()) {
+          log.info("warcExport buffer empty");
+          log.info("Warcs read:"+docsWarcRead +" arcs read:"+docsArcRead);
+          return totalRead;
+        }
+      }
+
+      // There is content. Read up to max requested
+      int read = entryStreams.get(0).read(b, off, toRead);
+      if (read == -1) { // The entryStream is empty. Remove it and go to the next
+        try {
+          entryStreams.get(0).close();
+        } catch (Exception e) {
+          log.warn("Error closing entryStream", e);
+        }
+        entryStreams.remove(0);
+        continue;
+      }
+
+      // We got some content. Update counters and loop to try and fill the input buffer fully
+      totalRead += read;
+      off += read;
     }
-
-    if (inputBuffer.isEmpty()) {
-      log.info("warcExport buffer empty");
-      log.info("Warcs read:"+docsWarcRead +" arcs read:"+docsArcRead);
-      return -1;
-    }
-
-    // Get first element of the List
-    byte[] bytes = inputBuffer.get(0);
-    // Get the byte corresponding to the index and post increment the current
-    // index
-    byte result = bytes[index++];
-    if (index >= bytes.length) {
-      // It was the last index of the byte array so we remove it from the list
-      // and reset the current index
-      inputBuffer.remove(0);
-      index = 0;
-    }
-
-    return 0xff & result; //We are not in ascii anymore.
+    return totalRead;
   }
 
   public StreamingSolrWarcExportBufferedInputStream(SolrGenericStreaming solrClient, int maxRecords) {
@@ -65,8 +81,6 @@ public class StreamingSolrWarcExportBufferedInputStream extends InputStream{
 
   private void loadMore() {
     try {
-      inputBuffer = new ArrayList<>();
-
       if (docsWarcRead > maxRecords) { //Stop loading more
         log.info("Max documents reached. Stopping loading more documents");
         return;
@@ -82,61 +96,99 @@ public class StreamingSolrWarcExportBufferedInputStream extends InputStream{
         String source_file_path = (String) doc.getFieldValue("source_file_path");
         long offset = (Long) doc.getFieldValue("source_file_offset");
 
-        ArcEntry warcEntry= null;
-
-        if (source_file_path.toLowerCase().endsWith(".arc")  || source_file_path.toLowerCase().endsWith(".arc.gz")){
-          //log.info("skipping Arc record:"+source_file_path);
-          try{
-            warcEntry = ArcParserFileResolver.getArcEntry(source_file_path,offset);
-          }
-          catch(Exception e){ //This will only happen if warc file is not found etc. Should not happen for real.
-            log.warn("Error loading arc:"+source_file_path,e);
-            continue;
-          }
-
-
-          String warcHeader = ArcHeader2WarcHeader.arcHeader2WarcHeader(warcEntry);
-          inputBuffer.add(warcHeader.getBytes(WarcParser.WARC_HEADER_ENCODING));          
-          docsArcRead++;
-        }
-        else{
-          try{
-            warcEntry = ArcParserFileResolver.getArcEntry(source_file_path,offset);
-          }
-          catch(Exception e){ //This will only happen if warc file is not found etc. Should not happen for real.
-            log.warn("Error loading warc:"+source_file_path,e);
-            continue;
-          }
-
-          docsWarcRead++;
-
-          String warc2HeaderEncoding = warcEntry.getContentEncoding();
-          Charset charset = Charset.forName(WarcParser.WARC_HEADER_ENCODING); //Default if none define or illegal charset
-
-          if (warc2HeaderEncoding != null){
-            try{
-              charset = Charset.forName(warc2HeaderEncoding);
-            }
-            catch (Exception e){
-              if (!"binary".equals(warc2HeaderEncoding)){ //This is not a real encoding
-                log.warn("unknown charset:"+warc2HeaderEncoding);
-              }
-            }
-          }         
-
-          inputBuffer.add(warcEntry.getHeader().getBytes(charset));                           
+        ArcEntry warcEntry = addHeadersReturnEntry(source_file_path, offset);
+        if (warcEntry == null) {
+          log.warn(String.format(Locale.ROOT, "Unable to resolve (W)ARC entry %s#%d for %s",
+                                 source_file_path, offset, doc.getFieldValue("id")));
+          continue;
         }
 
         //Do this for both arc/warc 
         if ( warcEntry.getBinary().length > 0){
-          inputBuffer.add(warcEntry.getBinary());
+          entryStreams.add(warcEntry.getBinaryLazyLoad());
         }
-        inputBuffer.add("\r\n\r\n".getBytes(WarcParser.WARC_HEADER_ENCODING) );              
+        entryStreams.add(new ByteArrayInputStream("\r\n\r\n".getBytes(WarcParser.WARC_HEADER_ENCODING)) );
 
       }      
     } catch (Exception e) {
+      log.error("Unhandled exception in loadMore", e);
       e.printStackTrace();
     }
 
+  }
+
+  /**
+   * Resolve the given (W)ARC-file, extract the headers and add them to the entryStreams.
+   * @param source_file_path the name of the (W)ARC-file.
+   * @param offset           the offset for the record in the (W)ARC-file.
+   * @return a representation of the (W)ARC file.
+   */
+  private ArcEntry addHeadersReturnEntry(String source_file_path, long offset) throws IOException  {
+    ArcEntry warcEntry;
+
+    // ARC
+    if (source_file_path.toLowerCase().endsWith(".arc") || source_file_path.toLowerCase().endsWith(".arc.gz")){
+      //log.info("skipping Arc record:"+source_file_path);
+      try{
+        warcEntry = ArcParserFileResolver.getArcEntry(source_file_path, offset);
+      }
+      catch(Exception e){ //This will only happen if warc file is not found etc. Should not happen for real.
+        log.warn("Error loading arc:"+source_file_path,e);
+        return null;
+      }
+
+
+      String warcHeader = ArcHeader2WarcHeader.arcHeader2WarcHeader(warcEntry);
+      // The header is (normally) fairly small, so we hold it in memory
+      try {
+        entryStreams.add(new ByteArrayInputStream(warcHeader.getBytes(WarcParser.WARC_HEADER_ENCODING)));
+      } catch (UnsupportedEncodingException e) {
+        String message = String.format(Locale.ROOT, "UnsupportedEncodingException for fixed charset '%s' while adding " +
+                                                    "headers for %s#%d. This should not happen",
+                                       WarcParser.WARC_HEADER_ENCODING, source_file_path, offset);
+        log.warn(message, e);
+        throw new RuntimeException(message, e);
+      }
+      docsArcRead++;
+    } else {
+      try{
+        warcEntry = ArcParserFileResolver.getArcEntry(source_file_path,offset);
+      }
+      catch(Exception e){ //This will only happen if warc file is not found etc. Should not happen for real.
+        log.warn("Error loading warc:"+source_file_path,e);
+        return null;
+      }
+
+
+      String warc2HeaderEncoding = warcEntry.getContentEncoding();
+      Charset charset = Charset.forName(WarcParser.WARC_HEADER_ENCODING); //Default if none define or illegal charset
+
+      if (warc2HeaderEncoding != null){
+        try{
+          charset = Charset.forName(warc2HeaderEncoding);
+        }
+        catch (Exception e){
+          if (!"binary".equals(warc2HeaderEncoding)){ //This is not a real encoding
+            log.warn("unknown charset:"+warc2HeaderEncoding);
+          }
+        }
+      }
+
+      // The header is (normally) fairly small, so we hold it in memory
+      entryStreams.add(new ByteArrayInputStream(warcEntry.getHeader().getBytes(charset)));
+      docsWarcRead++;
+    }
+    return warcEntry;
+  }
+
+  @Override
+  public void close() {
+    for (InputStream entryStream: entryStreams) {
+      try {
+        entryStream.close();
+      } catch (Exception e) {
+        log.error("Error closing cached entryStream during outer close()", e);
+      }
+    }
   }
 }
