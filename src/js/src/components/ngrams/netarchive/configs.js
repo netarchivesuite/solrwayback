1f8dc87fe543b2f02e7a01c004beb200480b76d7
jorntx <jt@kb.dk>
2020-11-03 07:08:57 +0100

    fixed wrong path in Tomcat when searching from chart

--- src/js/src/components/ngrams/netarchive/configs.js
+++ src/js/src/components/ngrams/netarchive/configs.js
@@ -1,5 +1,11 @@
 export default {
   SERVICE_URL : 'services/search/',
   START_YEAR : '1998',
-  END_YEAR: '2021'
+  END_YEAR: '2021',
+  BASE_SEARCH_URL: () => {
+      let currentUrl = window.location
+      let baseUrl = `${currentUrl.protocol}//${currentUrl.host}/${currentUrl.pathname.split('/')[1]}`
+      let searchPrefix = currentUrl.pathname.split('/')[1] === 'search' ? '' : '/search'
+      return `${baseUrl}${searchPrefix}`
+  }
 }
--- src/js/src/components/ngrams/searchHelper.js
+++ src/js/src/components/ngrams/searchHelper.js
@@ -1,9 +1,7 @@
+import Configs from './netarchive/configs'
 export default {
     handleSearch(queryFromClick, yearFromClick) {
-      let currentUrl = window.location
-      let baseUrl = `${currentUrl.protocol}//${currentUrl.host}/${currentUrl.pathname.split('/')[1]}`
-      let searchPrefix = currentUrl.pathname.split('/')[1] === 'search' ? '' : 'search'
-      const url = `${baseUrl}${searchPrefix}?query=${encodeURIComponent(queryFromClick)}&grouping=false&imgSearch=false&offset=0&urlSearch=false&facets=%26fq%3Dcrawl_year%3A"${yearFromClick}"` 
+      const url = `${Configs.BASE_SEARCH_URL()}?query=${encodeURIComponent(queryFromClick)}&grouping=false&imgSearch=false&offset=0&urlSearch=false&facets=%26fq%3Dcrawl_year%3A"${yearFromClick}"` 
       window.open(url, '_blank')
     }
 }
