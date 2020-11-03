743fb611eb11e30c77ed8722d1f0aba3515b455a
jorntx <jt@kb.dk>
2020-11-03 08:23:09 +0100

    Using base path from tomcat config for search from chart

--- src/js/src/components/ngrams/netarchive/configs.js
+++ src/js/src/components/ngrams/netarchive/configs.js
@@ -1,11 +1,10 @@
+import APP_CONFIGS from '../../../configs'
 export default {
   SERVICE_URL : 'services/search/',
   START_YEAR : '1998',
   END_YEAR: '2021',
   BASE_SEARCH_URL: () => {
-      let currentUrl = window.location
-      let baseUrl = `${currentUrl.protocol}//${currentUrl.host}/${currentUrl.pathname.split('/')[1]}`
-      let searchPrefix = currentUrl.pathname.split('/')[1] === 'search' ? '' : '/search'
-      return `${baseUrl}${searchPrefix}`
+      let searchPrefix = window.location.pathname.split('/')[1] === 'search' ? '' : 'search'
+      return `${APP_CONFIGS.playbackConfig.solrwaybackBaseURL}${searchPrefix}`
   }
 }
