e29ac429f416d7764dfcb60a833f5e632c1d068f
Toke Eskildsen <te@ekot.dk>
2021-04-06 15:33:41 +0200

    Make the config printer script runnable from anywhere

--- src/bundle/indexing/show_warc_config.sh
+++ src/bundle/indexing/show_warc_config.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
- java -cp warc-indexer-3.1.0-KB-SNAPSHOT-jar-with-dependencies.jar uk.bl.wa.util.ConfigPrinter
+
+pushd ${BASH_SOURCE%/*} > /dev/null
+
+java -cp warc-indexer-3.1.0-KB-SNAPSHOT-jar-with-dependencies.jar uk.bl.wa.util.ConfigPrinter
 
