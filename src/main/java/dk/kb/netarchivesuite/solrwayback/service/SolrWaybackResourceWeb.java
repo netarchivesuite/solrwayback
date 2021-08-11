767363cbccdadff4138ebb70c2748e389151fda2
Thomas Egense <teg@kb.dk>
2021-08-11 14:44:31 +0200

    cleanup

--- src/main/java/dk/kb/netarchivesuite/solrwayback/service/SolrWaybackResourceWeb.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/service/SolrWaybackResourceWeb.java
@@ -83,10 +83,8 @@ public class SolrWaybackResourceWeb {
       String sw_javascript=
       " self.addEventListener('fetch', function(event) { "+   
       "  url = event.request.url; "+
-      "  console.log('Serviceworker got url:'+url); "+
-      "  console.log('1'); "+
-      "  if (url.startsWith('http') && !url.startsWith('https://solrwb-test.kb.dk:4000/')){ " +   
-      "      console.log('2'); "+ 
+      "  console.log('Serviceworker got url:'+url); "+      
+      "  if (url.startsWith('http') && !url.startsWith('https://solrwb-test.kb.dk:4000/')){ " +    
       "      console.log('Found leak url:'+url); "+        
       "     newUrl = 'https://solrwb-test.kb.dk:4000/solrwayback/web/20210121153119/'+url; "+                 
       "     console.log('forwarding live leak url to:'+newUrl); "+ 
@@ -94,7 +92,6 @@ public class SolrWaybackResourceWeb {
       "       fetch(newUrl)); "+                            
       "   } "+
       "  else{ "+
-      "      console.log('3'); "+
       "      console.log('not forwarding url'); "+
       "  } "+
       " }); ";
