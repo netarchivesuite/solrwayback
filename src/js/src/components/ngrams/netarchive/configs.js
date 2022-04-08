f1d8df902f56dada0782d1f66b2f5a048cef3788
jorntx <jt@kb.dk>
2022-04-08 12:30:18 +0200

    Dynamic end year in ngram

--- src/js/src/components/ngrams/netarchive/configs.js
+++ src/js/src/components/ngrams/netarchive/configs.js
@@ -1,7 +1,7 @@
 import APP_CONFIGS from '../../../configs'
 export default {
   SERVICE_URL : 'services/search/',
-  END_YEAR: '2021',
+  END_YEAR: new Date().getFullYear().toString(),
   BASE_SEARCH_URL: () => {
       let searchPrefix = window.location.pathname.split('/')[1] === 'search' ? '' : 'search'
       return `${APP_CONFIGS.playbackConfig.solrwaybackBaseURL}${searchPrefix}`
