1e86e125d1bcbcbb4076cc69fcada12684830305
jorntx <jt@kb.dk>
2022-04-08 12:41:26 +0200

    Fix 1-off bug in end year

--- src/js/src/components/ngrams/netarchive/configs.js
+++ src/js/src/components/ngrams/netarchive/configs.js
@@ -1,7 +1,7 @@
 import APP_CONFIGS from '../../../configs'
 export default {
   SERVICE_URL : 'services/search/',
-  END_YEAR: new Date().getFullYear().toString(),
+  END_YEAR: (new Date().getFullYear() + 1).toString(),
   BASE_SEARCH_URL: () => {
       let searchPrefix = window.location.pathname.split('/')[1] === 'search' ? '' : 'search'
       return `${APP_CONFIGS.playbackConfig.solrwaybackBaseURL}${searchPrefix}`
