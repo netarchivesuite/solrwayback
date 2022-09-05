5d4b1d17ff0def6a56d907965d8a1afec6d7387d
Toke Eskildsen <toes@kb.dk>
2022-09-05 16:52:57 +0200

    Add (untested) timeProximity to SolrGenericStreaming

--- src/main/java/dk/kb/netarchivesuite/solrwayback/solr/SolrGenericStreaming.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/solr/SolrGenericStreaming.java
@@ -15,6 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 import static org.apache.commons.lang3.StringUtils.join;
 
@@ -24,6 +25,8 @@ import static org.apache.commons.lang3.StringUtils.join;
 public class SolrGenericStreaming {
 
   private static final int DEFAULT_PAGESIZE = 1000; // Due to Solr boolean queries limit
+  public static final String DEFAULT_SORT = "score desc, id asc";
+
   private static SolrClient solrServer;
   private final Logger log = LoggerFactory.getLogger(SolrGenericStreaming.class);
   private final boolean expandResources;
@@ -37,7 +40,53 @@ public class SolrGenericStreaming {
   private SolrDocumentList undelivered = null; // Leftover form previous call to keep deliveries below pageSize
   private boolean hasFinished = false;
 
+  private String streamDeduplicateField = null; // If set, timeProximity is used
+  private Object lastStreamDeduplicateValue = null; // Used with timeProximity
+
   // TODO: Make graph traversal of JavaScript & CSS-includes with expandResources
+
+  /**
+   * Streams documents that are closest in time to crawl_time, removing duplicates.
+   *
+   * @param solrServerUrl complete address for a Solr server.
+   * @param fields        comma separated fields to export.
+   * @param expandResources     if true, embedded resources for HTML pages are extracted and added to the delivered
+   *                            lists of Solr Documents.
+   *                            Note: Indirect references (through JavaScript & CSS) are not followed.
+   *                            Note: Duplicate removal does not work with expandResources.
+   * @param idealTime           The time that the resources should be closest to, stated as a Solr timestamp
+   *                            {@code YYYY-MM-DDTHH:mm:SSZ}. Also supports {@code oldest} and {@code newest} as values.
+   * @param deduplicateField    The field to use for de-duplication. This is typically {@code url}.
+   * @param query         standard Solr query.
+   * @param filterQueries optional Solr filter queries. For performance, 0 or 1 filter query is recommended.
+   *                      If multiple filters are to be used, consider collapsing them into one:
+   *                      {@code ["foo", "bar"]} → {@code ["(foo) AND (bar)"]}.
+   */
+  // TODO: When https://github.com/ukwa/webarchive-discovery/issues/214 gets implemented it should be possible to use Last-Modied/Date from HTTP headers instead of crawl_date
+  public static SolrGenericStreaming timeProximity(String solrServerUrl, List<String> fields, boolean expandResources,
+                                                   String idealTime, String deduplicateField,
+                                                   String query, String... filterQueries) {
+    // Extra steps for timeProximity:
+    // 1) Construct sort "<deduplicateField> asc, abs(sub(ms(2014-01-03T11:56:58Z), crawl_date)) asc")
+    // 2) Keep track of latest received deduplicateField. When the value changes, accept the document and
+    //    remember the new value for future deduplication
+    String it = idealTime;
+    if ("newest".equals(idealTime)) {
+      it = "9999-12-31T23:59:59Z";
+    } else if ("oldest".equals(idealTime)) {
+      it = "0001-01-01T00:00:01Z";
+    } else if (!ISO_TIME.matcher(idealTime).matches()) {
+      throw new IllegalArgumentException(
+              "The idealTime '" + idealTime + "' does not match 'oldest', 'newest' or 'YYYY-MM-DDTHH:mm:SSZ");
+    }
+    String sort = String.format(Locale.ROOT, "%s asc, abs(sub(ms(%s), crawl_date)) asc", deduplicateField, it);
+    SolrQuery totalQuery = buildBaseQuery(DEFAULT_PAGESIZE, fields, expandResources, query, filterQueries, sort);
+    SolrGenericStreaming stream = new SolrGenericStreaming(solrServerUrl, totalQuery, expandResources, false);
+    // TODO: Validate that deduplicateField does not need to be part of fields
+    stream.streamDeduplicateField = deduplicateField;
+    return stream;
+  }
+  private static final Pattern ISO_TIME =Pattern.compile("[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]T[012][0-9]:[0-5][0-9]Z");
 
   /**
    * Default page size 1000, expandResources=false and avoidDuplicates=false.
@@ -49,7 +98,7 @@ public class SolrGenericStreaming {
    *                      {@code ["foo", "bar"]} → {@code ["(foo) AND (bar)"]}.
    */
   public SolrGenericStreaming(String solrServerUrl, List<String> fields, String query, String... filterQueries) {
-    this(solrServerUrl, buildBaseQuery(DEFAULT_PAGESIZE, fields, false, query, filterQueries),
+    this(solrServerUrl, buildBaseQuery(DEFAULT_PAGESIZE, fields, false, query, filterQueries, DEFAULT_SORT),
          false, false);
   }
 
@@ -64,7 +113,7 @@ public class SolrGenericStreaming {
    */
   public SolrGenericStreaming(String solrServerUrl, String fields, String query, String... filterQueries) {
     this(solrServerUrl, buildBaseQuery(DEFAULT_PAGESIZE, Arrays.asList(fields.split(", *")), false,
-                                       query, filterQueries), false, false);
+                                       query, filterQueries, DEFAULT_SORT), false, false);
   }
 
   /**
@@ -85,7 +134,7 @@ public class SolrGenericStreaming {
   public SolrGenericStreaming(
           String solrServerUrl, int pageSize, List<String> fields, boolean expandResources, boolean avoidDuplicates,
           String query, String... filterQueries) {
-    this(solrServerUrl, buildBaseQuery(pageSize, fields, expandResources, query, filterQueries),
+    this(solrServerUrl, buildBaseQuery(pageSize, fields, expandResources, query, filterQueries, DEFAULT_SORT),
          expandResources, avoidDuplicates);
   }
 
@@ -113,13 +162,14 @@ public class SolrGenericStreaming {
   }
 
   private static SolrQuery buildBaseQuery(
-          int pageSize, List<String> fields, boolean expandResources, String query, String[] filterQueries) {
+          int pageSize, List<String> fields, boolean expandResources, String query, String[] filterQueries,
+          String sort) {
     List<String> finalFields = getFields(fields, expandResources);
     SolrQuery solrQuery = new SolrQuery();
     solrQuery.set("facet", "false"); //very important. Must overwrite to false. Facets are very slow and expensive.
     solrQuery.set("stats", "false");
     solrQuery.add("fl", join(finalFields, ","));
-    solrQuery.add("sort","score desc, id asc");
+    solrQuery.add("sort", sort);
     solrQuery.setRows(pageSize);
     solrQuery.setQuery(query);
     if (filterQueries != null) {
@@ -145,6 +195,10 @@ public class SolrGenericStreaming {
     QueryResponse rsp = solrServer.query(solrQuery, METHOD.POST);
     cursorMark = rsp.getNextCursorMark();
     SolrDocumentList documents = rsp.getResults();
+
+    if (streamDeduplicateField != null) {
+      documents = streamDeduplicate(documents);
+    }
 
     if (expandResources) {
       documents = expandResources(documents);
@@ -235,6 +289,25 @@ public class SolrGenericStreaming {
         encountered.add(getID(documents.get(i)));
       }
     }
+    return documents;
+  }
+
+  /**
+   * Streaming deduplication where the incoming documents are expected to be in order.
+   */
+  private SolrDocumentList streamDeduplicate(SolrDocumentList documents) {
+    List<SolrDocument> unique = new ArrayList<>(documents.size());
+    for (SolrDocument doc: documents) {
+      if (lastStreamDeduplicateValue == null ||
+          !lastStreamDeduplicateValue.equals(doc.getFieldValue(streamDeduplicateField))) {
+          lastStreamDeduplicateValue = doc.getFieldValue(streamDeduplicateField);
+          unique.add(doc);
+      } else {
+        duplicatesRemoved++;
+      }
+    }
+    documents.clear();
+    documents.addAll(unique);
     return documents;
   }
 
