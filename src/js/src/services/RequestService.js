7085ed16e34032573b5da61d407de0e79b253366
jorntx <jt@kb.dk>
2022-09-05 12:28:31 +0200

    Encode url param in normalize url request

--- src/js/src/services/RequestService.js
+++ src/js/src/services/RequestService.js
@@ -112,6 +112,7 @@ function uploadFileRequest(fileData) {
 }
 
 function getNormalizedUrlSearch(query, facets, options) {
+  console.log(query)
   const url = `services/frontend/util/normalizeurl/?url=${encodeURIComponent(query)}`
   return axios.get(
     url).then(response => {
