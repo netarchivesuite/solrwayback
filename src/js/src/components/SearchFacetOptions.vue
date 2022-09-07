d6bf85e006c82467c15df22a40473e8f283776ab
Victor Harbo Johnston <vhol@kb.dk>
2022-09-07 16:05:27 +0200

    Add info markers

--- src/js/src/components/SearchFacetOptions.vue
+++ src/js/src/components/SearchFacetOptions.vue
@@ -14,8 +14,10 @@
              :class="facetIndex % 2 === 0 ? 'facetItem' : 'facetCount'"
              @click="facetIndex % 2 === 0 ? applyFacet(facetCategory[0], facet) : null">
           {{ facetIndex % 2 === 0 ? facet || "Unknown" : "(" + facet.toLocaleString("en") + ")" }}
+          <span v-if="filter(facetObj, facet)" class="buttonExplanation" :title="getDescription(facetObj, facet)">&#9432;</span> 
         </div>
-        <div v-show="extraFacetLoading === facetCategory[0]" class="extraFacetsloading" />
+        <div v-show="extraFacetLoading === facetCategory[0]" class="extraFacetsloading">
+        </div>
         <!-- here we're excluding the crawl_year facets, because OP don't want a show more on those -->
         <div v-if="facetCategory[1].length >= 20 && facetCategory[0] !== 'crawl_year'" class="moreFacets">
           <div v-if="facetCategory[1].length > 20" class="facetArrow up">
@@ -43,6 +45,16 @@ import { requestService } from '../services/RequestService'
 export default {
   name: 'SearchFacetOptions',
   mixins: [HistoryRoutingUtils],
+  data() {
+    return {
+      facetObj: [
+        {facet: 'kb.dk', description: 'kb.dk is our own domain, it is harvested perfectly!'},
+        {facet: 'dr.dk', description: 'dr.dk is nicely harvested through professional cooperation'},
+        {facet: 'text', description: 'This facet shows only collected text'},
+        {facet: 'elvium.com', description: 'Does this work?'}
+      ]
+    }
+  },
   computed: {
     ...mapState({
       searchAppliedFacets: state => state.Search.searchAppliedFacets,
@@ -53,6 +65,9 @@ export default {
       facetLoading: state => state.Search.facetLoading,
       extraFacetLoading: state => state.Search.extraFacetLoading
     }),
+    description() {
+      return this.facetObj.facet === facetItem ? 'Yes' : 'No'
+    }
   },
   methods: {
     ...mapActions('Search', {
@@ -91,10 +106,22 @@ export default {
           facets[item].length !== 0 ? fate = true : null 
         }) 
       } return fate
+    },
+    filter(facetObj, name) {
+      const inArray = facetObj.some(el => el.facet === name)
+      return inArray
+    },
+    getDescription(facetObj, facet) {
+      let output = ''
+      facetObj.map(obj => {
+          if (obj.facet == facet) {
+            output += obj.description
+          }
+      })
+      return output
     }
   }
 }
-
 </script>
 
     
