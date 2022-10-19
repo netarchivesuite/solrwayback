348a75568e24b0e8cf8766a209ad4649ecb5dbcc
Toke Eskildsen <te@ekot.dk>
2022-10-19 23:09:13 +0200

    Add alternative etImagesFromWebpage implementation

--- src/main/java/dk/kb/netarchivesuite/solrwayback/export/ContentStreams.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/export/ContentStreams.java
@@ -17,14 +17,21 @@ package dk.kb.netarchivesuite.solrwayback.export;
 import com.google.common.base.Functions;
 import dk.kb.netarchivesuite.solrwayback.facade.Facade;
 import dk.kb.netarchivesuite.solrwayback.service.dto.ArcEntryDescriptor;
+import dk.kb.netarchivesuite.solrwayback.service.dto.IndexDoc;
+import dk.kb.netarchivesuite.solrwayback.solr.NetarchiveSolrClient;
 import dk.kb.netarchivesuite.solrwayback.solr.SolrGenericStreaming;
 import dk.kb.netarchivesuite.solrwayback.util.CollectionUtils;
 import dk.kb.netarchivesuite.solrwayback.util.Processing;
 import dk.kb.netarchivesuite.solrwayback.util.SolrUtils;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -66,6 +73,34 @@ public class ContentStreams {
 
         // Mix the two streams, 4 direct images for each 1 image derived from a page
         return CollectionUtils.interleave(Arrays.asList(directImages, htmlImages), Arrays.asList(4, 1));
+    }
+
+    /**
+     * Resolves {@code crawl_date} and {@code links_images} for the webpage with the given source_file_path and
+     * source_file_offset then resolves all the images in {@code links_images}. If there are multiple images with
+     * the same {@code url_norm}, the one closest to {@code crawl_date} is returned.
+     *
+     * If the resource designated by the given path and offset is not a webpage, an empty stream is returned.
+     * @param source_file_path   path for the resource, as indexed in Solr.
+     * @param source_file_offset offset for the resource, as indexed in Solr.
+     * @return the images linked from the page.
+     * @throws Exception if the webpage could not be found or image resolving failed.
+     */
+    public Stream<ArcEntryDescriptor> getImagesFromWebpage(String source_file_path, long source_file_offset)
+            throws Exception {
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.set("facet", "false");
+        solrQuery.add("fl", "crawl_date,links_images");
+        solrQuery.setQuery("source_file_path:\"" + source_file_path + "\" AND source_file_offset:" + source_file_offset);
+        solrQuery.setRows(1);
+        QueryResponse rsp = NetarchiveSolrClient.query(solrQuery, false);
+        SolrDocumentList docs = rsp.getResults();
+
+        if (docs.isEmpty()) {
+            throw new IOException("Could not find arc entry with source file path='" + source_file_path +
+                                  "' and offset=" + source_file_offset);
+        }
+        return createHTMLImageCallback(docs.get(0), Integer.MAX_VALUE).call();
     }
 
     /**
