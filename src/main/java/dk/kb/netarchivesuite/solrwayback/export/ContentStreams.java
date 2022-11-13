d63c1f4c2a6c9241bcea529e9bc4f264aab45104
Toke Eskildsen <te@ekot.dk>
2022-11-13 21:24:25 +0100

    Make eager evaluation more compact

--- src/main/java/dk/kb/netarchivesuite/solrwayback/export/ContentStreams.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/export/ContentStreams.java
@@ -143,14 +143,15 @@ public class ContentStreams {
                 fields(SolrUtils.arcEntryDescriptorFieldList). // Contains hash used for uniqueness
                 timeProximityDeduplication(isotime, "url_norm").
                 maxResults(maxImages); // No sense in returning more than maxImages from a sub-request
-        return () -> {
+        return () -> request.stream().collect(Collectors.toList()).stream();
+/*        return () -> {
 //            long startNS = System.nanoTime();
             List<SolrDocument> result = request.stream().
 //                    filter(new ThroughputTracker("htmlPageNearImages", "image", log, 10)).
                     collect(Collectors.toList());
             return result.stream();
 
-        };
+        }; */
     }
 
     /**
