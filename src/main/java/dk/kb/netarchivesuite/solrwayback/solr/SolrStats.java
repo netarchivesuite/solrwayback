8f425f4e4a583c783ed9609fa4368531f86eb8fb
Victor Harbo Johnston <vhol@kb.dk>
2023-04-20 10:54:05 +0200

    Add method to extract percentiles for fields

--- src/main/java/dk/kb/netarchivesuite/solrwayback/solr/SolrStats.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/solr/SolrStats.java
@@ -2,6 +2,7 @@ package dk.kb.netarchivesuite.solrwayback.solr;
 
 import com.google.gson.Gson;
 import dk.kb.netarchivesuite.solrwayback.properties.PropertiesLoaderWeb;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.slf4j.Logger;
@@ -20,7 +21,6 @@ public class SolrStats {
     final List<String> interestingTextFields = Arrays.asList("links", "domain", "elements_used", "content_type",
                                                                 "content_language", "links_images", "type");
     final List<String> otherNumericFields = Arrays.asList("score", "status_code", "source_file_offset", "_version_", "wayback_date");
-
     /**
      * Get standard solr stats for all fields given
      *
@@ -30,7 +30,6 @@ public class SolrStats {
      * @return all standard stats for all fields from query as a JSON string.
      */
     public static String getStatsForFields(String query, List<String> filters, List<String> fields){
-        //TODO: Should contain a check, that the values are actually numeric and not anything else.
         SolrQuery solrQuery = new SolrQuery();
         solrQuery.setQuery(query);
 
@@ -39,6 +38,27 @@ public class SolrStats {
                 solrQuery.setGetFieldStatistics(field);
             } else {
                 log.warn("Stats can not be shown for field: " + field + " as it is not present in properties.");
+            }
+        }
+
+        QueryResponse response = NetarchiveSolrClient.query(solrQuery, true);
+        Gson gson = new Gson();
+        String stats = gson.toJson(response.getFieldStatsInfo().values());
+        return stats;
+    }
+
+    public static String getPercentilesForFields(String query, List<String> percentiles, List<String> fields){
+        SolrQuery solrQuery = new SolrQuery();
+        solrQuery.setQuery(query);
+
+        String allPercentiles = StringUtils.join(percentiles, ",");
+        String percentileQuery = "{!percentiles='" + allPercentiles + "'}";
+
+        for (String field: fields) {
+            if (PropertiesLoaderWeb.STATS.contains(field)){
+                solrQuery.setGetFieldStatistics(percentileQuery + field);
+            } else {
+                log.warn("Percentile stats can not be shown for field: " + field + " as it is not present in properties.");
             }
         }
 
