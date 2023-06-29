830a8ebaffbd2a0b50e9b56dd5122fa68b473fe3
Toke Eskildsen <toes@kb.dk>
2023-06-29 21:49:27 +0200

    Add background scanning with rescan

--- src/main/java/dk/kb/netarchivesuite/solrwayback/interfaces/AutoFileResolver.java
+++ src/main/java/dk/kb/netarchivesuite/solrwayback/interfaces/AutoFileResolver.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * Optional FileLocationResolver interface implementation.
@@ -26,18 +27,28 @@ import java.util.Map;
  * <pre>
  warc.file.resolver.class=AutoFileResolver
  warc.file.resolver.parameters.autoresolver.roots=/home/sw/warcs1,/netmounts/colfoo
+ warc.file.resolver.parameters.autoresolver.pattern=.*[.]w?arc([.]gz)?
+ warc.file.resolver.parameters.autoresolver.rescan.enabled=false
+ warc.file.resolver.parameters.autoresolver.rescan.seconds=1200
  </pre>
- *
+ * Only the {@code roots} parameter is mandatory.
+ * {@code pattern}, {@code rescan.enabled} and {@code rescan.seconds} had the
+ * defaults shown above
  */
-// TODO: Optional rescan interval
 // TODO: Optional "scan on unknown file" trigger
-// TODO: Adjustable pattern for matching WARCs to avoid garbage in the map
 // TODO: Unit test (use the WARCs in test/resources)
 @SuppressWarnings("unused")
-public class AutoFileResolver implements ArcFileLocationResolverInterface {
+public class AutoFileResolver implements ArcFileLocationResolverInterface, Runnable {
     private static final Logger log = LoggerFactory.getLogger(AutoFileResolver.class);
 
-    public static final String ROOTS_KEY = "autoresolver.roots"; // Prefixed warc.file.resolver.parameters
+    // Prefixed warc.file.resolver.parameters
+    public static final String  ROOTS_KEY = "autoresolver.roots";
+    public static final String  PATTERN_KEY = "autoresolver.pattern";
+    public static final String  PATTERN_DEFAULT = ".*[.]w?arc([.]gz)?";
+    public static final String  RESCAN_ENABLED_KEY = "autoresolver.rescan.enabled";
+    public static final boolean RESCAN_ENABLED_DEFAULT = false;
+    public static final String  RESCAN_SECONDS_KEY = "autoresolver.rescan.seconds";
+    public static final long    RESCAN_SECONDS_DEFAULT = 60;
 
     public enum STATE { initializing, scanning, dormant }
 
@@ -46,6 +57,10 @@ public class AutoFileResolver implements ArcFileLocationResolverInterface {
      */
     private Map<String, String> WARCS = new HashMap<>();
     private final List<Path> roots = new ArrayList<>();
+    private Pattern filePattern;
+    private boolean rescanEnabled;
+    private long rescanSeconds;
+    private Thread scanThread;
     private STATE state = STATE.initializing;
 
     /**
@@ -81,13 +96,45 @@ public class AutoFileResolver implements ArcFileLocationResolverInterface {
             log.error(message);
             throw new IllegalStateException(message);
         }
-        log.info("Assigned root folders " + roots);
+
+        filePattern = parameters.containsKey(PATTERN_KEY) ?
+                Pattern.compile(parameters.get(PATTERN_KEY)):
+                Pattern.compile(PATTERN_DEFAULT);
+        rescanEnabled = parameters.containsKey(RESCAN_ENABLED_KEY) ?
+                Boolean.parseBoolean(parameters.get(RESCAN_ENABLED_KEY)) :
+                RESCAN_ENABLED_DEFAULT;
+        rescanSeconds = parameters.containsKey(RESCAN_SECONDS_KEY) ?
+                Long.parseLong(parameters.get(RESCAN_SECONDS_KEY)) :
+                RESCAN_SECONDS_DEFAULT;
+
+        log.info("Assigned parameters for {}", this);
     }
 
     @Override
     public void initialize() {
-        log.info("Starting initial scan. This might take a while");
-        scanFull(); // TODO: Should probably be in a background thread
+        log.info("Creating and activating scan thread");
+        scanThread = new Thread(this, "AutoFileResolverThread");
+        scanThread.setDaemon(true); // Shut down the Thread automatically on war redeploy
+        scanThread.start();
+    }
+
+    @SuppressWarnings("BusyWait")
+    @Override
+    public void run() {
+        long scanCount = 0;
+        do { // 1 scan is guaranteed, even if rescanEnabled is false
+            state = scanCount++ == 0 ? STATE.initializing : STATE.scanning;
+            scanFull();
+            state = STATE.dormant;
+
+            if (rescanEnabled) {
+                try {
+                    Thread.sleep(rescanSeconds*1000);
+                } catch (InterruptedException e) {
+                    log.warn("Interrupted while sleeping. This should not happen but is not fatal");
+                }
+            }
+        } while (rescanEnabled);
     }
 
     /**
@@ -95,7 +142,6 @@ public class AutoFileResolver implements ArcFileLocationResolverInterface {
      * The collected mappings only takes effect when the scan has fully completed.
      */
     private synchronized void scanFull() {
-        state = STATE.scanning;
         final long startTime = System.currentTimeMillis();
         log.info("Starting scan for (W)ARC from roots {}. This might take a while", roots);
 
@@ -104,12 +150,16 @@ public class AutoFileResolver implements ArcFileLocationResolverInterface {
             scanRoot(root, newWARCs);
         }
         WARCS = newWARCs;
-        state = STATE.dormant;
 
         log.info("Finished scan for WARCs from {} roots in {} seconds. Number of registered files: {}",
                  roots.size(), (System.currentTimeMillis() - startTime) / 1000, WARCS.size());
     }
 
+    /**
+     * Scan recursively from the given {@code path} and add WARC files to the {@code warcs} map.
+     * @param path  folder to scan from.
+     * @param warcs map for collecting WARC to folder mappings.
+     */
     private void scanRoot(Path path, Map<String, String> warcs) {
         final String location = path.toString(); // TODO: Check that it does not end in '/'
         try (DirectoryStream<Path> pathEntries = Files.newDirectoryStream(path)) {
@@ -118,11 +168,15 @@ public class AutoFileResolver implements ArcFileLocationResolverInterface {
                     scanRoot(pathEntry, warcs);
                 } else {
                     String filename = pathEntry.getFileName().toString();
-                    if (warcs.containsKey(filename)) {
-                        log.warn("The WARC name '{}' in folder '{}' is already present in folder '{}'",
-                                 filename, location, warcs.get(filename));
+                    if (filePattern.matcher(filename).matches()) {
+                        if (warcs.containsKey(filename)) {
+                            log.warn("The WARC name '{}' in folder '{}' is already present in folder '{}'",
+                                     filename, location, warcs.get(filename));
+                        }
+                        warcs.put(filename, location);
+                    } else {
+                        log.debug("Scanner encountered non-matching file '{}'", filename);
                     }
-                    warcs.put(filename, location);
                 }
             });
         } catch (AccessDeniedException e) {
@@ -134,6 +188,15 @@ public class AutoFileResolver implements ArcFileLocationResolverInterface {
 
     @Override
     public ArcSource resolveArcFileLocation(String source_file_path){
+        while (state == STATE.initializing) {
+            try {
+                // Busy wait is not the clean way, but this is only relevant during startup
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                log.debug("Interrupted while waiting for state change. This is not problematic");
+            }
+        }
+
         String fileName = new File(source_file_path).getName();
         String value = WARCS.get(fileName);
 
@@ -147,5 +210,17 @@ public class AutoFileResolver implements ArcFileLocationResolverInterface {
      */
     public STATE getState() {
         return state;
+    }
+
+    @Override
+    public String toString() {
+        return "AutoFileResolver(" +
+               "roots='" + roots + "'" +
+               ", filePattern=" + filePattern +  "'" +
+               ", rescanEnabled=" + rescanEnabled +
+               ", rescanSeconds=" + rescanSeconds +
+               ", state=" + state +
+               ", #WARCS=" + WARCS.size() +
+               '}';
     }
 }
