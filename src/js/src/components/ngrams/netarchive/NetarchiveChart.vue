f133f42b8d7c9a18d54ec8f02dbda78e7b294869
Jesper Lauridsen <jela@kb.dk>
2025-09-18 11:08:35 +0200

    Should take care of the chart.js and vue-charts.js problems for now.

--- src/js/src/components/ngrams/netarchive/NetarchiveChart.vue
+++ src/js/src/components/ngrams/netarchive/NetarchiveChart.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <div class="chart-container">
-      <button v-if="this.ngramStore.datasets.length > 0" class="download" @click="downloadOpen = !downloadOpen">
+      <button
+        v-if="ngramStore.datasets.length > 0"
+        class="download"
+        @click="downloadOpen = !downloadOpen"
+      >
         DOWNLOAD
       </button>
-      <line-chart v-if="this.ngramStore.datasets.length > 0"
-                  :chart-data="datacollection"
-                  :options="options"
-                  :chart-id="'netarchive-chart'"
-                  :height="150" />
+      <canvas
+        id="ngramChart"
+        width="600"
+        height="300"
+      />
     </div>
     <div v-if="downloadOpen">
       <export-data @close-exporter="downloadOpen = false" />
@@ -17,94 +21,105 @@
 </template>
 
 <script>
-  
-  import LineChart from '../chartsCore/chartEngines/LineChart'
-  import ChartHelpers from '../chartsCore/chartHelpers'
-  import ExportData from '../exporterCSV/ExportData.vue'
-  import { mapStores } from 'pinia'
-import { useNgramStore } from '../../../store/ngram.store'
+import { drawSingleLineChart } from '../chartsCore/chartEngines/LineChart';
+import ChartHelpers from '../chartsCore/chartHelpers';
+import ExportData from '../exporterCSV/ExportData.vue';
+import { mapStores } from 'pinia';
+import { useNgramStore } from '../../../store/ngram.store';
 
-  export default {
-    name: 'NetarchiveChart',
-    components: {
-      LineChart,
-      ExportData
+export default {
+  name: 'NetarchiveChart',
+  components: { ExportData },
+  data() {
+    return {
+      datacollection: {},
+      downloadOpen: false,
+      chartInstance: null,
+      options: ChartHelpers.getChartOptions(null, this.scale),
+    };
+  },
+  computed: {
+    ...mapStores(useNgramStore),
+  },
+watch: {
+  'ngramStore.datasets': {
+    handler() {
+      this.fillData();
     },
-
-    data() {
-      return {
-        datacollection: {},
-        downloadOpen: false,
-        options: ChartHelpers.getChartOptions(null, this.scale)
-       }
-    },
-
-    computed: {
-    // ...mapState({
-    //   query: state => state.Ngram.query,
-    //   datasets: state => state.Ngram.datasets,
-    //   searchType:state => state.Ngram.searchType,
-    //   labels: state => state.Ngram.labels,
-    //   scale: state => state.Ngram.timeScale
-    // })
-    ...mapStores(useNgramStore)
-    },
-
-    watch: {
-      'ngramStore.datasets': function (newVal) {
-        this.fillData(newVal)
-      },
-      
-      'ngramStore.searchType': function (newVal) {
-        this.options = ChartHelpers.getChartOptions(newVal, this.ngramStore.scale)
-      },
-      'ngramStore.scale': function (newVal) {
-        this.options = ChartHelpers.getChartOptions(this.ngramStore.searchType, this.ngramStore.scale)
-      }
-    },
-    
-    methods: {
-      fillData () {
-        this.datacollection = {
-          rawLabels: this.ngramStore.labels,
-          labels: ChartHelpers.getChartLabels(this.ngramStore.labels, this.ngramStore.scale),
-          datasets: ChartHelpers.getChartDataSet(this.ngramStore.datasets)
-        }
-      }
-    }
+    deep: true
+  },
+  'ngramStore.searchType'() {
+    this.options = ChartHelpers.getChartOptions(this.ngramStore.searchType, this.ngramStore.scale);
+    this.redrawChart();
+  },
+  'ngramStore.scale'() {
+    this.options = ChartHelpers.getChartOptions(this.ngramStore.searchType, this.ngramStore.scale);
+    this.redrawChart();
   }
+},
+
+
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      if (!this.ngramStore.datasets.length) return;
+      console.log("new data, yayayayayay!");
+      // Prepare datacollection
+      this.datacollection = {
+        labels: ChartHelpers.getChartLabels(this.ngramStore.labels, this.ngramStore.scale),
+        datasets: ChartHelpers.getChartDataSet(this.ngramStore.datasets),
+      };
+
+      console.log(this.datacollection);
+
+      // Redraw chart whenever data updates
+      this.redrawChart();
+    },
+
+    redrawChart() {
+  if (!this.datacollection || !this.datacollection.datasets.length) return;
+
+  this.chartInstance = drawSingleLineChart(
+    'ngramChart',
+    this.datacollection.labels,
+    this.datacollection.datasets, // <-- pass all datasets now
+    'Count'
+  );
+}
+
+  },
+};
 </script>
 
 <style lang="scss">
-  @use '../../../assets/styles/charts.scss'; 
+@use '../../../assets/styles/charts.scss';
 
-  .download {
-    border: 2px solid var(--secondary-bg-color);
-    border-radius: 0;
-    background-color: white;
-    color: var(--secondary-bg-color);
-    padding: 5px 10px 5px 10px;
-    box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.25);
-    cursor: pointer;
-    box-sizing: border-box;
-    transition: all 0.2s linear 0s;
-    text-align:center;
-    width:fit-content;
-    position: relative;
-    left: 50%;
-    transform: translateX(-50%);
-  }
+.download {
+  border: 2px solid var(--secondary-bg-color);
+  border-radius: 0;
+  background-color: white;
+  color: var(--secondary-bg-color);
+  padding: 5px 10px 5px 10px;
+  box-shadow: 0px 3px 6px 0px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+  box-sizing: border-box;
+  transition: all 0.2s linear 0s;
+  text-align: center;
+  width: fit-content;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+}
 
-  .download:hover {
-    border: 2px solid white;
-    background-color: var(--secondary-bg-color);
-    color: white;
+#ngramChart {
+  max-height:300px !important;
 
-
-
-  }
-
+}
+.download:hover {
+  border: 2px solid white;
+  background-color: var(--secondary-bg-color);
+  color: white;
+}
 </style>
-
-
-
