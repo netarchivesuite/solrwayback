a3abd857cc331c8a3005c42c9660bf1c8bbb5fa6
VictorHarbo <vholesen@hotmail.com>
2025-09-22 13:56:35 +0200

    Make the bat script work on windows

--- src/bundle/solrwayback_bundle.bat
+++ src/bundle/solrwayback_bundle.bat
@@ -11,6 +11,10 @@ IF /I "%~1"=="start" (
     call solr-9\bin\solr.cmd start -c -m 4g
 
     echo Starting SolrWayback in tomcat...
+
+    REM Set CATALINA_HOME to the "tomcat-9" folder inside the current directory
+    set "CATALINA_HOME=%cd%\tomcat-9"
+
     call tomcat-9\bin\startup.bat
 
     echo.
@@ -20,10 +24,13 @@ IF /I "%~1"=="start" (
 
 IF /I "%~1"=="stop" (
     echo Stopping SolrWayback in tomcat...
+
+    REM Set CATALINA_HOME to the "tomcat-9" folder inside the current directory
+    set "CATALINA_HOME=%cd%\tomcat-9"
     call tomcat-9\bin\shutdown.bat
 
     echo Stopping solr...
-    call solr-9\bin\solr.cmd stop
+    call solr-9\bin\solr.cmd stop -all
 
     echo.
     echo Stopped SolrWayback
